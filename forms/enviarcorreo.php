2b742fa1910436021d904ad5cc0367a2ff073c92
Mauricio Alcalá <a_mauricio@live.com>
2021-04-12 20:15:41 -0700

    Update enviarcorreo.php

--- forms/enviarcorreo.php
+++ forms/enviarcorreo.php
@@ -8,21 +8,21 @@ require("../lib/PHPMailer/src/SMTP.php");
  $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
  $mail->SMTPAuth = true; // authentication enabled
  $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
- $mail->Host = "mail.asesorsolarindependiente.com";
+ $mail->Host = "mail.cnrnoticias.tv";
  $mail->Port = 465; //465  or 587
  $mail->IsHTML(true);
- $mail->Username = "contacto@asesorsolarindependiente.com";
- $mail->Password = "Robertosandoval";
- $mail->SetFrom("contacto@asesorsolarindependiente.com");
- $mail->Subject = "Contacto Desde : asesorsolarindependiente.com";
+ $mail->Username = "contacto@cnrnoticias.tv";
+ $mail->Password = "CNRNoticiasEstatal73";
+ $mail->SetFrom("contacto@cnrnoticias.tv");
+ $mail->Subject = "Contacto Desde : cnrnoticias.tv";
  
  $name = $_POST['name'];
- $phone = $_POST['phone'];
  $email = $_POST['email'];
+ $phone = $_POST['phone'];
  $message = $_POST['message'];
  
- $mail->Body = "Nombre: {$name} {$lastname} <br> Telefono : {$phone} <br> Email: {$email} <br> Mensaje {$message}";
- $mail->AddAddress("contacto@asesorsolarindependiente.com");
+ $mail->Body = "Nombre: {$name} <br> Email: {$email} <br> Teléfono : {$phone} <br> Mensaje : {$message}";
+ $mail->AddAddress("contacto@cnrnoticias.tv");
  if(!$mail->Send()) {
  echo "Mailer Error: " . $mail->ErrorInfo;
  } else {
